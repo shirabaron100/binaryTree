4ad608b8b3e1d2cd1b1536b092c7ab03d315ffb9
shirabaron100 <shirayba@gmail.com>
2019-03-25 19:57:11 +0200

    add tree.cpp

---
+++ Tree.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "Tree.hpp"
+
+
+using namespace ariel;
+
+  
+   Tree::Tree(){   
+   }
+    void Tree::insert(int i){
+    }
+    void Tree::remove (int i){
+    }
+    int Tree::size(){
+        return 0;
+    }
+    bool Tree::contains(int i){
+        return true;
+    }
+    int Tree::root(){
+        return 0;
+    }
+    int Tree::parent (int i){
+        return 0;
+    }
+    int Tree::left(int i){
+        return 0;
+    }
+    int Tree::right(int i){
+        return 0;
+    }
+    void Tree::print(){     
+    }
+
